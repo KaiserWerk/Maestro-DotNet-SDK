cd6e4c736452aa9f881f9b268320c5f79453c108
Robin Kaiser <web@r-k.mx>
2021-12-20 22:24:41 +0100

    now works due to proper JsonIgnore Attribute

--- Maestro/Registrant.cs
+++ Maestro/Registrant.cs
@@ -7,6 +7,6 @@ namespace Maestro
     {
         [JsonPropertyName("id")] public string AppId { get; set; }
         [JsonPropertyName("address")] public string Address { get; set; }
-        [JsonPropertyName("last_ping")] public DateTime LastPing { get; set; }
+        [JsonPropertyName("last_ping")] [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)] public DateTime LastPing { get; set; }
     }
 }
